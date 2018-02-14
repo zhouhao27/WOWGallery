da811dc03c6777126a3cf198a20628c350a4c3a3
Zhou Hao <zhou.hao.27@gmail.com>
2018-02-14 14:09:28 +0800

    Add carthage

---
+++ Example/WOWGallery/WOWGallery.h
@@ -0,0 +1,19 @@
+//
+//  WOWGallery.h
+//  WOWGallery
+//
+//  Created by Zhou Hao on 14/2/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WOWGallery.
+FOUNDATION_EXPORT double WOWGalleryVersionNumber;
+
+//! Project version string for WOWGallery.
+FOUNDATION_EXPORT const unsigned char WOWGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WOWGallery/PublicHeader.h>
+
+
